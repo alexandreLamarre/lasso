857efd81f520263856a70ea2487fc72b5208f448
Alexandre Lamarre <alexandre.lamarre@suse.com>
2025-02-26 17:43:17 -0500

    propagator improvements / shared controller tracer ctx setup
    Signed-off-by: Alexandre Lamarre <alexandre.lamarre@suse.com>

--- pkg/tracing/propagator.go
+++ pkg/tracing/propagator.go
@@ -2,20 +2,28 @@ package tracing
 
 import (
 	"context"
+	"sync"
 
 	"github.com/sirupsen/logrus"
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/apimachinery/pkg/runtime"
 )
 
 type ObjectCarrier struct {
+	mu     *sync.Mutex
 	Object runtime.Object
 }
 
+func HasParent(ctx context.Context) bool {
+	sp := trace.SpanContextFromContext(ctx)
+	return sp.IsValid() && sp.IsRemote()
+}
+
 func Extract(ctx context.Context, obj runtime.Object) context.Context {
-	return otel.GetTextMapPropagator().Extract(ctx, NewObjectCarrier(obj))
+	return otel.GetTextMapPropagator().Extract(ctx, NewObjectCarrier(obj.DeepCopyObject()))
 }
 
 func Inject(ctx context.Context, obj runtime.Object) {
@@ -29,12 +37,15 @@ func (o ObjectCarrier) isNil() bool {
 func NewObjectCarrier(obj runtime.Object) ObjectCarrier {
 	return ObjectCarrier{
 		Object: obj,
+		mu:     &sync.Mutex{},
 	}
 }
 
 var _ propagation.TextMapCarrier = (*ObjectCarrier)(nil)
 
 func (o ObjectCarrier) Get(key string) string {
+	o.mu.Lock()
+	defer o.mu.Unlock()
 	if o.isNil() {
 		return ""
 	}
@@ -43,16 +54,26 @@ func (o ObjectCarrier) Get(key string) string {
 		logrus.Tracef("Error getting key %s from object carrier: %s", key, err)
 		return ""
 	}
+	logrus.Tracef("accessor : %v", accessor)
+	if accessor == nil {
+		return ""
+	}
 	existingAnn := accessor.GetAnnotations()
+	logrus.Tracef("existingAnn : %v", existingAnn)
+	if existingAnn == nil {
+		return ""
+	}
 	return existingAnn[key]
 }
 
 func (o ObjectCarrier) Set(key string, value string) {
+	o.mu.Lock()
+	defer o.mu.Unlock()
 	if o.isNil() {
 		return
 	}
 	accessor, err := meta.Accessor(o.Object)
-	if err != nil {
+	if err != nil || accessor == nil {
 		logrus.Tracef("Error setting key %s from object carrier: %s", key, err)
 		return
 	}
@@ -65,13 +86,15 @@ func (o ObjectCarrier) Set(key string, value string) {
 }
 
 func (o ObjectCarrier) Keys() []string {
+	o.mu.Lock()
+	defer o.mu.Unlock()
 	if o.isNil() {
-		return nil
+		return []string{}
 	}
 	accessor, err := meta.Accessor(o.Object)
-	if err != nil {
+	if err != nil || accessor == nil {
 		logrus.Tracef("Error getting keys from object carrier: %s", err)
-		return nil
+		return []string{}
 	}
 	existingAnn := accessor.GetAnnotations()
 	keys := make([]string, 0, len(existingAnn))
